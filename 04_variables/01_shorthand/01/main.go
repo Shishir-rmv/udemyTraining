8f0fd719c37e2d5b5d212cfc1658b8b9dc5cef2d
Shishir Verma <shishir.rmv@gmail.com>
2017-02-13 18:14:03 +0530

    Added code from from section 5 - variables

---
+++ 04_variables/01_shorthand/01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "Shishir"
+	c := 3.14
+	d := true
+
+	fmt.Printf("%v \t %T\n", a, a)
+	fmt.Printf("%v \t %T\n", b, b)
+	fmt.Printf("%v \t %T\n", c, c)
+	fmt.Printf("%v \t %T\n", d, d)
+}
